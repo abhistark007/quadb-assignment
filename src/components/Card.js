a61dda2f8287a8a84c42020eea8db1a2475ecb69
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-06-07 00:18:24 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Card.css';
+import { Link } from 'react-router-dom';
 
-function Card({name,image,score}) {
+function Card({name,image,score,summary}) {
   return (
-    <div className='card'>
+    <Link to={"/summary"} state={{ name: name,image:image,summary:summary }}>
+        <div className='card'>
         <img src={image} alt={name??"NA"} />
         <div className='card__content'>
             <p>{name??"NA"}</p>
             <div className="score">{score.toPrecision(2)}</div>
         </div>
     </div>
+    </Link>
   )
 }
 
